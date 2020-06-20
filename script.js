66b9154492cf3bd2c7864b65f37f514a79be7f00
relaxed snail <ricardogsolis@gmail.com>
2020-06-19 22:09:13 -0400

    created variables using jQuery

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // created date using moment.js
 let currentDay = $("#currentDay").append(moment().format("dddd, MMM Do, YYYY"));
 
+const userInput = $(".userInput");
 
-
-
+const saveBtn = $(".saveBtn");
